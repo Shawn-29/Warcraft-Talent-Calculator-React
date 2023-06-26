5554cea81e7d9ce73b19aeb13982451c7e5c38a4
shawn-29 <shawnwebdev@gmail.com>
2023-06-25 19:59:47 -0700

    Updated glyph modal CSS to fill available screen space on smaller devices.

--- src/components/ui/modal/styles.js
+++ src/components/ui/modal/styles.js
@@ -14,15 +14,23 @@ const Wrapper = styled.article`
         background-color: inherit;
         border-radius: var(--border-radius);
         box-shadow: 0 0 4px 5px var(--clr-black);
-        max-height: 95%;
+        max-height: 100%;
         max-width: var(--max-width);
         left: 50%;
         overflow-x: hidden;
         overflow-y: scroll;
         position: fixed;
-        top: 1rem;
+        top: 0;
         transform: translateX(-50%);
-        width: 85%;
+        width: 100%;
+    }
+
+    @media screen and (min-width: 576px) {
+        .content-container {
+            max-height: 95%;
+            top: 1rem;
+            width: 90%;
+        }
     }
 
     .close-btn {
